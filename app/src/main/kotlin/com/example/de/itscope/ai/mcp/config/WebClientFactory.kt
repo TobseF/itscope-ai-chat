2ba292377b47af5afd339535e9635f68c417a594
Tobse Fritz <Tobse4Git@gmail.com>
2025-10-14 23:47:37 +0200

    Fix ITscope API response to big
    Fixed the Exception: DataBufferLimitException: Exceeded limit on max bytes to buffer : 262144. When searching for a product.

--- app/src/main/kotlin/com/example/de/itscope/ai/mcp/config/WebClientFactory.kt
+++ app/src/main/kotlin/com/example/de/itscope/ai/mcp/config/WebClientFactory.kt
@@ -7,42 +7,52 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.codec.json.Jackson2JsonDecoder
 import org.springframework.http.codec.json.Jackson2JsonEncoder
+import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
-
 
 /**
  * Provides a configured [WebClient].
  */
 @Configuration
 class WebClientFactory {
-
     @Bean
     fun createWebClient(config: WebClientConfig): WebClient {
-        return WebClient.builder()
+        /**
+         * 4 MB
+         */
+        val sizeMB = 16 * 1024 * 1024
+
+        val strategies =
+            ExchangeStrategies
+                .builder()
+                .codecs { codecs ->
+                    codecs.defaultCodecs().maxInMemorySize(sizeMB)
+                }.build()
+
+        return WebClient
+            .builder()
             .baseUrl(config.baseUrl)
             .defaultCookie("cookieKey", "cookieValue")
             .defaultHeaders { it.setBasicAuth(config.accountId, config.apiKey) }
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .defaultHeader(HttpHeaders.USER_AGENT,config.apiKey)
+            .defaultHeader(HttpHeaders.USER_AGENT, config.apiKey)
+            .exchangeStrategies(strategies)
             .codecs {
                 it.defaultCodecs().jackson2JsonEncoder(
                     Jackson2JsonEncoder(
                         Serializer.jacksonObjectMapper,
-                        MediaType.APPLICATION_JSON
-                    )
+                        MediaType.APPLICATION_JSON,
+                    ),
                 )
                 it.defaultCodecs().jackson2JsonDecoder(
                     Jackson2JsonDecoder(
                         Serializer.jacksonObjectMapper,
-                        MediaType.APPLICATION_JSON
-                    )
+                        MediaType.APPLICATION_JSON,
+                    ),
                 )
-            }
-            .build()
+            }.build()
     }
 
     @Bean
     fun getObjectMapper() = Serializer.jacksonObjectMapper
-
-
 }
